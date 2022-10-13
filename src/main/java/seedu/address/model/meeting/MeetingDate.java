ab862f8b719dc039e7157e14064fed4eb7279e69
alexandermula <alexandersinghabdullah@gmail.com>
2022-10-14 01:21:05 +0800

    Update MeetingDate to store meeting date as variable

--- src/main/java/seedu/address/model/meeting/MeetingDate.java
+++ src/main/java/seedu/address/model/meeting/MeetingDate.java
@@ -15,7 +15,7 @@ public class MeetingDate {
 
     public static final String MESSAGE_CONSTRAINTS = "Date should be in the form of dd MMM yyyy";
 
-    public final String value;
+    public final String meetingDate;
 
     /**
      * Constructs a {@code Date}. Meeting date can be null
@@ -26,7 +26,7 @@ public class MeetingDate {
         if (date != null && !date.isEmpty()) {
             checkArgument(isValidMeetingDate(date), MESSAGE_CONSTRAINTS);
         }
-        this.value = date;
+        this.meetingDate = date;
 
     }
 
@@ -50,19 +50,19 @@ public class MeetingDate {
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof MeetingDate // instanceof handles nulls
-                && (value != null && value.equals(((MeetingDate) other).value))); // state check
+                && (meetingDate != null && meetingDate.equals(((MeetingDate) other).meetingDate))); // state check
     }
 
     @Override
     public int hashCode() {
-        return value.hashCode();
+        return meetingDate.hashCode();
     }
 
     /**
      * Format state as text for viewing.
      */
     public String toString() {
-        return value;
+        return meetingDate;
     }
 
 }
